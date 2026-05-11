aaefe241a4802e3d63ba639eb70f6bb6a461c5ba
careayurvritta-creator <careayurvritta-creator@users.noreply.github.com>
2026-05-11 11:44:07 +0530

    Add Tools, Blog, Privacy, Terms - removed Programs (causing issue)

--- App.tsx
+++ App.tsx
@@ -5,9 +5,12 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import ServiceDetail from './pages/ServiceDetail';
-import Programs from './pages/Programs';
 import Booking from './pages/Booking';
 import Insurance from './pages/Insurance';
+import Tools from './pages/Tools';
+import Blog from './pages/Blog';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Terms from './pages/Terms';
 
 const App: React.FC = () => {
   return (
@@ -18,9 +21,12 @@ const App: React.FC = () => {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/services/:id" element={<ServiceDetail />} />
-          <Route path="/programs" element={<Programs />} />
           <Route path="/booking" element={<Booking />} />
           <Route path="/insurance" element={<Insurance />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+          <Route path="/terms" element={<Terms />} />
         </Routes>
       </Layout>
     </HashRouter>
