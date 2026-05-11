3f5bf8b5a75b959d792484eaeb27f10012a9ada4
careayurvritta-creator <careayurvritta-creator@users.noreply.github.com>
2026-05-11 11:27:54 +0530

    Working version - only Home page with Layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-
-// Pages only - no SEOHead, CookieConsent, MobileCTABar
 import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import ServiceDetail from './pages/ServiceDetail';
-import Programs from './pages/Programs';
-import Tools from './pages/Tools';
-import Booking from './pages/Booking';
-import Insurance from './pages/Insurance';
-import Blog from './pages/Blog';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Terms from './pages/Terms';
 
 const App: React.FC = () => {
   return (
@@ -21,16 +9,6 @@ const App: React.FC = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/services/:id" element={<ServiceDetail />} />
-          <Route path="/programs" element={<Programs />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/insurance" element={<Insurance />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<Terms />} />
         </Routes>
       </Layout>
     </HashRouter>
