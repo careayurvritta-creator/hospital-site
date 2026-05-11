82baf11983839544afcab4b769247e9f5f781613
careayurvritta-creator <careayurvritta-creator@users.noreply.github.com>
2026-05-11 11:19:50 +0530

    Test with Home page

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Layout>
-        <div style={{ padding: 40, background: '#F5F0E8', minHeight: '100vh' }}>
-          <h1 style={{ color: '#009688' }}>Ayurvritta Hospital</h1>
-          <p>Layout loaded successfully!</p>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Layout>
     </HashRouter>
   );
