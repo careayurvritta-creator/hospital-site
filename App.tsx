9bc3ddb9fd94e896781deae01e70fd1b1ae82c85
careayurvritta-creator <careayurvritta-creator@users.noreply.github.com>
2026-05-11 11:29:35 +0530

    Add About page

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Layout>
     </HashRouter>
